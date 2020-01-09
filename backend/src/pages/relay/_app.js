9f84f360a0d0906d699f08ec4d5fc456b2ebb8c0
Fabio M. Costa <fabiomcosta@gmail.com>
2020-01-08 22:20:15 -0800

    I dont know.

---
+++ backend/src/pages/relay/_app.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { QueryRenderer, fetchQuery } from 'react-relay';
+import { initEnvironment, createEnvironment } from '../../relay/environment';
+
+function App({ Component, variables = {}, relayData }) {
+  const environment = createEnvironment(
+    relayData,
+    JSON.stringify({
+      queryID: Component.query ? Component.query().params.name : undefined,
+      variables
+    })
+  );
+  return (
+    <QueryRenderer
+      environment={environment}
+      query={Component.query}
+      variables={variables}
+      render={({ error, props }) => {
+        if (error) return <div>{error.message}</div>;
+        else if (props) return <Component {...props} />;
+        return <div>Loading</div>;
+      }}
+    />
+  );
+}
+
+App.getInitialProps = async ({ Component, router, ctx }) => {
+  const { variables } = Component.getInitialProps
+    ? await Component.getInitialProps(ctx)
+    : {};
+
+  try {
+    if (initEnvironment && Component.query) {
+      const { environment, relaySSR } = initEnvironment();
+      await fetchQuery(environment, Component.query, variables);
+      return {
+        variables,
+        relayData: await relaySSR.getCache()
+      };
+    }
+  } catch (e) {
+    console.log(e);
+  }
+
+  return {
+    variables
+  };
+};
+
+export default App;
--- backend/src/relay/environment.js
+++
@@ -1,9 +0,0 @@
-import { Environment, Network, RecordSource, Store } from 'relay-runtime';
-import { fetchQuery } from '../utils';
-
-const environment = new Environment({
-  network: Network.create(fetchQuery),
-  store: new Store(new RecordSource())
-});
-
-export default environment;
